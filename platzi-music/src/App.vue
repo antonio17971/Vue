39f9bbbcf319b2971fb9251803737bbcae5d107a
antonio quesada <jose17971@outlook.es>
2018-09-24 00:31:11 -0600

    Initial commit

---
+++ platzi-music/src/App.vue
@@ -0,0 +1,35 @@
+
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar Canciones")
+          a.button.is-info.is-large buscar
+          a.button.is-danger.is-large x
+
+        
+      .container
+        .columns
+          .cloumn
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  },
+  methods:{
+  }
+
+  
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss';
+</style>
