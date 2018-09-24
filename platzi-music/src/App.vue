c8e1714d617a16f7570fe17e28e39e6092b8110a
antonio quesada <jose17971@outlook.es>
2018-09-24 00:53:44 -0600

    no hay cambios

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -4,26 +4,41 @@
     section.section
       nav.nav.has-shadow
         .container
-          input.input.is-large(type="text", placeholder="Buscar Canciones")
-          a.button.is-info.is-large buscar
+          input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") buscar
           a.button.is-danger.is-large x
-
+          p 
+            small {{searchMessage}}
         
       .container
-        .columns
-          .cloumn
+        .columns.space
+          .cloumn(v-for="t in tracks") {{t.name}} - {{t.artista}} 
 
 </template>
 
 <script>
+const tracks=[
+  {name:'la cucaracha', artista:'don juan'},
+  {name:'maria jose', artista:'don arturo'},
+  {name:'yo no se ', artista:'jajajajajaj'},
+]
 export default {
   name: 'app',
   data () {
     return {
-      
+      searchQuery:'',
+      tracks:[]
     }
   },
   methods:{
+    search(){
+      this.tracks=tracks
+    }
+  },
+  computed:{
+    searchMessage(){
+      return "Encontrados: " + this.tracks.length
+    }
   }
 
   
@@ -32,4 +47,8 @@ export default {
 
 <style lang="scss">
 @import './scss/main.scss';
+
+.space{
+  margin-top: 50px
+}
 </style>
